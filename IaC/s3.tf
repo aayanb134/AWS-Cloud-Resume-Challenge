8887c3b84d851a4e4b319490aac5f0ba813afe81
AayanButt <aayanb134@gmail.com>
2023-09-08 18:06:29 +0100

    S3
    wrote S3 terraform file with relevant outputs

--- IaC/s3.tf
+++ IaC/s3.tf
@@ -47,7 +47,7 @@ resource "aws_s3_bucket_policy" "cloudfront_s3_policy" {
   })
 }
 
-# Outputs
+# Outputs ------------------------------------------------------------------------------------------------
 
 output "s3_bucket_arn" {
   description = "ARN of s3 bucket"
